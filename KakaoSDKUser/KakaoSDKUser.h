91de110236bae6973f9c766366cb745e0312210e
wannagohome <iwwuf7@gmail.com>
2020-09-24 16:20:16 +0900

    Initial Commit

---
+++ KakaoSDKUser/KakaoSDKUser.h
@@ -0,0 +1,19 @@
+//
+//  KakaoSDKUser.h
+//  KakaoSDKUser
+//
+//  Created by jinho jang on 2020/09/24.
+//  Copyright © 2020 People. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KakaoSDKUser.
+FOUNDATION_EXPORT double KakaoSDKUserVersionNumber;
+
+//! Project version string for KakaoSDKUser.
+FOUNDATION_EXPORT const unsigned char KakaoSDKUserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KakaoSDKUser/PublicHeader.h>
+
+
